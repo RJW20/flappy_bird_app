70dd8dd550264ff76abd783ab8cf29ea44274a42
RJW20 <robertjwatts84@outlook.com>
2024-02-22 13:56:03 +0000

    Implemented pipes methods

--- flappy_bird_app/pipes.py
+++ flappy_bird_app/pipes.py
@@ -3,6 +3,10 @@ from collections import deque
 from .pipe import Pipe
 
 
+#constants for all instances of Pipes that aren't referenced elsewhere
+SEPARATOR = 0.6
+
+
 class Pipes:
     """Double-ended queue containing the pipes."""
 
@@ -15,9 +19,20 @@ class Pipes:
         Populate deque, set at starting positions.
         """
 
+        self.items = deque([Pipe(0)])
+
     def update(self) -> None:
         """Move the pipes one frame to the left.
         
         Add pipe to the end of self.items if soon to be visible.
         Remove pipe at beginning of self.items if no longer visible.
         """
+
+        for pipe in self.items:
+            pipe.update()
+
+        if (end_pipe_position := self.items[-1].position) < 1:
+            self.items.append(Pipe(end_pipe_position + SEPARATOR))
+
+        if self.items[0].position < self.items[0].width * -1:
+            self.items.popleft()       
