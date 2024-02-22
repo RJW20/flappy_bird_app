253ef29ad8cbb6ad5888c64150363d65db01ba2a
RJW20 <robertjwatts84@outlook.com>
2024-02-22 13:51:52 +0000

    Add basic pipe functionality

--- flappy_bird_app/pipe.py
+++ flappy_bird_app/pipe.py
@@ -1,9 +1,21 @@
+import random
 
+
+#constants for all instances of Pipe that aren't referenced elsewhere
+MIN_HEIGHT = 0.05
+PAN_VELOCITY = -0.005
 
 
 class Pipe:
     """A pipe that can kill the bird."""
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, position: float) -> None:
+        self.position: float = position
+        self.width: float = 0.18
+        self.gap: float = 0.28
+        self.height: float = round(random.uniform(MIN_HEIGHT, 1 - MIN_HEIGHT - self.gap) * 100) / 100
 
+    def update(self) -> None:
+        """Move pipe by PAN_VELOCITY."""
+
+        self.position += PAN_VELOCITY
